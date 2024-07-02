f033ee3ae3c10ebb05bcb51334fe32183b7d6c4b
Lincy3777 <lincy.thomas@xaviers.edu.in>
2024-07-02 21:22:14 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -7,17 +7,28 @@ const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3300;
 // it will check the environment variable first if it doesnt find port var. then it will run on 3000
 
-//Assets
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.render('home');
-});//request on /                                                                                                                            
-
 // set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 console.log(path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+//Assets
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('home');
+});//request on /                                                                                                                            
+app.get('/cart',(req,res) => {
+    res.render('customers/cart');
+});
+
+app.get('/login',(req,res) => {
+    res.render('authentication/login');
+});
+
+app.get('/register',(req,res) => {
+    res.render('authentication/register');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
